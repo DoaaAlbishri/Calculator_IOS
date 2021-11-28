2ba1e7a715f4e44d3cceeb047b3ea1d57176eca1
Doaa Albishri <Duaa_ali@outlook.sa>
2021-11-28 10:55:34 +0000

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,106 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by admin on 28/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var total: Double = 0
+    var num1: Double = 0
+    var num2: Double = 0
+    
+    var bool :Bool = false
+    var op = ""
+    var emp = "not empty "
+    var zero = 2
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBOutlet weak var TotalLabel: UILabel!
+    
+    @IBAction func numbers(_ sender: UIButton) {
+        if(bool == false){
+            if(zero == 0){
+                TotalLabel.text = sender.titleLabel!.text!
+                zero = 2
+            }else{
+        TotalLabel.text = TotalLabel.text! + sender.titleLabel!.text!
+            }
+        total = Double(TotalLabel.text!)!
+        num1 = Double(TotalLabel.text!)!
+        }else{
+            if(emp == ""){
+            TotalLabel.text = sender.titleLabel!.text!
+                emp = "not empty "
+            }else{
+            TotalLabel.text =  TotalLabel.text! + sender.titleLabel!.text!
+            }
+            num2 = Double(TotalLabel.text!)!
+        }
+    }
+
+    @IBAction func operation(_ sender: UIButton) {
+        let operation = sender.titleLabel?.text
+        switch operation {
+        case "/": TotalLabel.text = "/"
+            op="/"
+        case "X": TotalLabel.text = "X"
+          //  emp = ""
+            op="*"
+        case "-": TotalLabel.text = "-"
+            op="-"
+          //  emp = ""
+        case "+": TotalLabel.text = "+"
+            op="+"
+          //  emp = ""
+        case "=":
+            switch op{
+            case "/": TotalLabel.text = String((num1/num2))
+                total =  Double(TotalLabel.text!)!
+                num1 = total
+            case "*": TotalLabel.text = String((num1*num2))
+                total =  Double(TotalLabel.text!)!
+                num1 = total
+            case "-": TotalLabel.text = String((num1-num2))
+                total =  Double(TotalLabel.text!)!
+                num1 = total
+            case "+": TotalLabel.text = String((num1+num2))
+                total =  Double(TotalLabel.text!)!
+                num1 = total
+            default: print("error")
+            }
+            bool = false
+            return
+        default: print("error")
+        }
+        bool = true
+        emp = ""
+    }
+    
+    
+    @IBAction func advanceOperation(_ sender: UIButton) {
+        let operation = sender.titleLabel?.text
+        switch operation{
+        case "C":TotalLabel.text = "0"
+            total=0
+            num1=0
+            num2=0
+            zero = 0
+            bool = false
+            return
+        case "%": TotalLabel.text = TotalLabel.text! + "%"
+        case "+/-":
+            var num = Double(TotalLabel.text!)!
+            TotalLabel.text = String(num * -1)
+        default:print("error")
+        }
+    }
+    
+}
+
